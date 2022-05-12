83d83aaa6b88d65943953ac2fd5be47d6c8a6fab
hhenrying <h.hernandez.ing@gmail.com>
2022-05-12 11:11:01 -0500

    crud basico en asp .net

---
+++ Controllers/CitasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUD.Datos;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class CitasController : Controller
+    {
+        private readonly ContextDB _context;
+
+        public CitasController(ContextDB context)
+        {
+            _context = context;
+        }
+
+        // GET: Citas
+        public async Task<IActionResult> Index()
+        {
+              return _context != null ? 
+                          View(await _context.Citas.ToListAsync()) :
+                          Problem("Entity set 'ContextDB.Citas'  is null.");
+        }
+
+        // GET: Citas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // GET: Citas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Citas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,Titulo,Fecha,Hora")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cita);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cita);
+        }
+
+        // GET: Citas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            return View(cita);
+        }
+
+        // POST: Citas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Titulo,Fecha,Hora")] Cita cita)
+        {
+            if (id != cita.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cita);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitaExists(cita.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cita);
+        }
+
+        // GET: Citas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // POST: Citas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Citas == null)
+            {
+                return Problem("Entity set 'ContextDB.Citas'  is null.");
+            }
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita != null)
+            {
+                _context.Citas.Remove(cita);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitaExists(int id)
+        {
+          return (_context.Citas?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Datos/ContextDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CRUD.Models;
+
+namespace CRUD.Datos
+{
+    public class ContextDB : DbContext
+    {
+        public ContextDB(DbContextOptions<ContextDB> options ):base(options)
+        {
+
+        }
+
+        public DbSet<Cita> Citas { get; set; }
+    }
+}
